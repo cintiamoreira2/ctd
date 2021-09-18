af84d07ddc065c0ded50478ce8416a6d288d9192
Cintia Rodrigues Moreira <Cintia1350@gmail.com>
2021-09-17 22:57:29 -0300

    Delete PI directory
    substituição para atualização

--- PI/Aula IV/IMC.js
+++
@@ -1,8 +0,0 @@
-const nome = 'José da Silva'
-let idade = 27;
-let peso = 83.5;
-let altura = 1.70;
-let plano = true;
-let imc = (peso / (altura ^ 2)).toFixed(2);
-
-console.log(nome, "tem", idade, "anos e seu indice de massa corporal é de", imc)
